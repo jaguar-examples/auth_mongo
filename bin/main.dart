bda1dc2e751c6d5bd3801f918b02411cb7d3b5c6
tejainece <tejainece@gmail.com>
2018-12-28 21:04:16 +0100

    Dev proxy server to serve html pages

--- bin/main.dart
+++ bin/main.dart
@@ -4,6 +4,7 @@ import 'package:server/server.dart';
 import 'package:jaguar_session_jwt/jaguar_session_jwt.dart';
 import 'package:server/models/models.dart';
 import 'package:server/auth/auth.dart';
+import 'package:jaguar_dev_proxy/jaguar_dev_proxy.dart';
 
 final jwtConfig = JwtConfig(
   "dgdfgdwsrtnretlkj6h5467kjh4567kjh32k4h532jh5435",
@@ -15,6 +16,7 @@ main() async {
       sessionManager: JwtSession(jwtConfig, io: SessionIoCookie()));
   server.userFetchers[ServerUser] = MyAuthFetcher();
   server.add(reflect(TodoApi()));
+  server.addRoute(getOnlyProxy("/*", "http://localhost:8000"));
 
   server.log.onRecord.listen(print);
   await server.serve(logRequests: true);
